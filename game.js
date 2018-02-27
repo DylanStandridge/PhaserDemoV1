6b79ae96b5037314fd19e6aa9f1bcec6b1cb7917
christopherwbushman <christopherwbushman@gmail.com>
2018-02-26 20:17:53 -0500

    added square and movement

--- game.js
+++ game.js
@@ -1,10 +1,34 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('square', 'assets/Images/square.png');
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function create() {
+  player = game.add.image(370, 270, 'square');
 }
 
 function update() {
+  if (cursors.left.isDown)
+  {
+      //  Move to the left
+      player.position.x = player.position.x - 5;
+  }
+  else if (cursors.right.isDown)
+  {
+      //  Move to the right
+       player.position.x = player.position.x + 5;
+  }
+  
+  else if (cursors.up.isDown)
+  {
+      //  Move to the left
+      player.position.y = player.position.y - 5;
+  }
+  else if (cursors.down.isDown)
+  {
+      //  Move to the right
+       player.position.y = player.position.y + 5;
+  }
 }
