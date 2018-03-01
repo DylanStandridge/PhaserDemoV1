9fa5dea2c2252b0ca50549d8f8671aadb7343bca
dylownius94 <dylownius94@gmail.com>
2018-02-28 20:48:44 -0500

    new assets

--- game.js
+++ game.js
@@ -2,11 +2,21 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
   game.load.image('square', 'assets/Images/square.png');
+  game.load.image('firstaid', 'assets/Images/firstaid.png');
+  game.load.image('star', 'assets/Images/star.png');
+
   cursors = game.input.keyboard.createCursorKeys();
 }
 
 function create() {
   player = game.add.image(370, 270, 'square');
+  hp = game.add.image(0, 0, 'firstaid');
+  hp1 = game.add.image(20, 0, 'firstaid');
+  hp2 = game.add.image(40, 0, 'firstaid');
+  
+  star = game.add.image(200, 200, 'star');
+
+
 }
 
 function update() {
